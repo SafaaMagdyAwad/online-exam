30072aa401e4b5568af7921987ff42ea410a0d85
Safaa Magdy <engsafaamagdy1024@gmail.com>
2026-01-14 20:29:47 +0200

    c

--- index.mjs
+++ index.mjs
@@ -35,4 +35,5 @@ mongoose.connect(uri)
   .then(() => console.log('MongoDB connected ✅'))
   .catch(err => console.error('MongoDB connection error ❌', err));
 const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log(`Server running on http://localhost:${port}`); });
+// app.listen(port, () => { console.log(`Server running on http://localhost:${port}`); });
+export default app;
